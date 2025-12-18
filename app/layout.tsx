c52d8696a787fba051b50deb8c7c7a2df46ecbd9
FrosttysBots <125443973+FrosttysBots@users.noreply.github.com>
2025-12-18 07:53:11 +0100

    Added Analytics for Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar"; // adjust path if yours is different
 import CursorGlow from "./components/CursorGlow";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: "SleazyG_27 Hub",
@@ -94,6 +95,7 @@ export default function RootLayout({
                 <CursorGlow />
                 {children}
                 <Footer />
+                <Analytics />
             </body>
         </html>
     );
